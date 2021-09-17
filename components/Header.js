ca3ba42b049c87a1098ebf92ec25e1d536c14dbe
Go7hic <gtfx0209@qq.com>
2021-09-17 16:20:57 +0800

    fix: logo

--- components/Header.js
+++ components/Header.js
@@ -70,7 +70,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
-                <Image src="logo.png" alt="" style={{ width: '24px', height: '24px' }} />
+                <Image src="logo.png" alt="logo" width={24} height={24} />
               </div>
             </a>
           </Link>
