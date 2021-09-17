de634ba520b0d5714ebd445f13b290b9832e14ca
Go7hic <gtfx0209@qq.com>
2021-09-17 16:18:35 +0800

    fix: del footer

--- components/Vercel.js
+++ components/Vercel.js
@@ -5,7 +5,7 @@ const Vercel = () => {
       target="_blank"
       rel="noreferrer"
       aria-label="Vercel"
-      style={{ display: "none" }}
+      style={{ display: 'none' }}
     >
       <svg
         width="135"
